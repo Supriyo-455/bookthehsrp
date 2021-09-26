b5ac312f15c71d333e2240bb93364ab21dcaa6c6
Supriyo Paul <paulsupriyo64nk@gmail.com>
2021-09-26 12:55:47 +0530

    registration_page

---
+++ app/src/main/java/com/example/bookthehsrp/ui/fragment/RegistrationFragment.kt
@@ -0,0 +1,12 @@
+package com.example.bookthehsrp.ui.fragment
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.example.bookthehsrp.R
+
+class RegistrationFragment : Fragment(R.layout.registration_fragment){
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
